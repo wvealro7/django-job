ee0e5a95a8ce5e6166a5df3c2b16e0bf2f93e53b
Meshari Alotaibi <wve.alro7@gmail.com>
2020-06-08 19:13:00 +0300

    style pages of website

--- project/urls.py
+++ project/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('pages.urls')),
     path('jobs/', include('jobs.urls')),
 ]
 
